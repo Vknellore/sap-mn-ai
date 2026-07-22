af881f9ca41ac9afc0206156e3104f9c5f45cb71
Vknellore <92936226+Vknellore@users.noreply.github.com>
2026-07-22 23:08:43 +0100

    Refactor next.config.js for Next.js configuration
    Refactor next.config.js to use a NextConfig object with additional settings for output and images.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-module.exports = { basePath: '/sap-mn-ai', assetPrefix: '/sap-mn-ai/', trailingSlash: true, }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Important for serving the site at https://Vknellore.github.io/sap-mn-ai
+  basePath: '/sap-mn-ai',
+  assetPrefix: '/sap-mn-ai/'
+}
+
+module.exports = nextConfig
